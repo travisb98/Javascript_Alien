7338e4f5a4deea2b78a2c52824560fd3f1feb79f
unknown <travisbostick@gmail.com>
2020-12-28 15:29:06 -0600

    created a working html page to customize for bonus

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,14 +1,15 @@
 // from data.js
 /////probably need to replace every use of "data" with tableData
+// var data = data;
 var tableData = data;
 
 //from html_codes.js
-var htmlcodes = codes;
+//need to create a functiontion that uses this dictionary to clean the data 
+var code_dict = codes;
 
-console.log(htmlcodes);
 
-// defines button
-var button = d3.select("#filter-btn");
+// defines the filter button
+var filt_button = d3.select("#filter-btn");
 
 //defines form
 
@@ -20,10 +21,8 @@ var tbody = d3.select("tbody");
 
 
 //handles the for submission and filter button clicks
-button.on("click",dataload);
-form.on("submit",dataload);
-
-
+filt_button.on("click",dateload);
+form.on("submit",dateload);
 
 
 //creates a list of the unique dates in the data set
@@ -34,58 +33,16 @@ for (let i=0;i<data.length;i++){
         uDates.push(data[i].datetime)
     }
 };
-// console.log(uDates)
 
 
-//// create a function that cleans the data
-
-
-
-
-/// create a function that accepts input and loads the data based on that date
-function dataload(){
-
-
-    // Prevent the page from refreshing(i think this will be needed when we implement the form)
-    d3.event.preventDefault();
-
-
-    //create the input object
-    var input = d3.select("#datetime");
-    
-    // grab the value from the input object
-    var input_value=input.property('value');
-
-
-
-    // create if statement to deal with blank input
-    //create if statement to deal with dates out of range
-    ////// find the oldest and youngest date in data, if the input is out of this range, return an alert message and exit the function
-    ///// just realized the data for "durationMinutes" and "comments" is inconsistent and contains encoding issues, need to clean
-
-
-
-    ////// replacing leading zeros from the month and day so the input is consistent
-    //replaces leading zero from month
-    input_value=input_value.replace(/\/0+/g, '/');
-
-    //replaces leading zero in date if it starts with a zero
-    if(input_value[0]==0){
-        input_value=input_value.slice(1,input_value.length)
-    };
-
-
-    // filter the data based on the input
-    var reqdata = data.filter(record => record.datetime==input_value);
-
-
-    /// loop through each record from the requested data
-
-    reqdata.forEach(result => {
+//function that will be used to append data to rows
+/// loop through each record from the requested data and create table data for each column from our data set
+function append_row_data(current_data){
+    current_data.forEach(result => {
 
         //creates a new row
         var row = tbody.append("tr");
-
+    
         // adds a row for each property in the data
         row.append("td").text(result.datetime);
         row.append("td").text(result.city);
@@ -94,16 +51,78 @@ function dataload(){
         row.append("td").text(result.shape);
         row.append("td").text(result.durationMinutes);
         row.append("td").text(result.comments);
-
     });
 
-    //console logs for testing
-    console.log("Running dataload function");
-    console.log(reqdata);
-    console.log("-------------");
-    console.log(input_value);
-    console.log(input_value[0]);
-
-    // console.log(input);
 };
 
+
+//// create a function that cleans the data using the html code dictionary
+
+
+
+
+/// create a function that accepts input and loads the data based on that date
+function dateload(){
+
+    // Prevent the page from refreshing(i think this will be needed when we implement the form)
+    d3.event.preventDefault();
+
+    //create the input object
+    var input = d3.select("#datetime");
+    
+    // grab the value from the input object
+    var input_value=input.property('value');
+
+    //replaces leading zero from month
+    input_value=input_value.replace(/\/0+/g, '/');
+
+    //replaces leading zero in date if it starts with a zero
+    if(input_value[0]==0){
+        input_value=input_value.slice(1,input_value.length)
+    };
+
+    // if the input is not included in our list of unique dates....
+    if (!uDates.includes(input_value)){
+
+        // if there was no input at all.....
+        if (input_value==""){
+            alert("input was blank, returning all data");
+            
+            // fill the table with all the data, no filter applied
+            append_row_data(data);
+        }
+        // ... the input entered was not blank or in the dataset
+        else{
+            ////////// this could be displayed better
+            alert(`There is no data for the date you entered. Please use one of the following dates
+            ${uDates}`)            
+        };
+    }
+    /// since the input entered was valid....
+    else{
+        // filter the data based on the input
+        var reqdata = data.filter(record => record.datetime==input_value);
+
+        /// loop through each record from the requested data and create table data for each column from our data set
+        append_row_data(reqdata);
+    };
+
+    //console logs for testing
+    console.log("Running dateload function");
+    console.log("-------------");
+// end of dateload function 
+};
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log(Object.keys(data[0]));
+
+// ///this just prints out each key
+// Object.keys(data[0]).forEach(x => {
+//     console.log(x);
+// });
